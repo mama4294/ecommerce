672e2035bae79db20b29399bd5966d6e53422e80
Matthew Malone <mama4294@colorado.edu>
2022-03-29 21:02:02 -0600

    Added home page route and navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+// Initial render: Render an element to the root.
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
