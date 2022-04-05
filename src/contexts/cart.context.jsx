5e8ec5a3bb1a379fb4e6b41aad42bc8879efb9ac
Matthew Malone <mama4294@colorado.edu>
2022-04-05 16:53:16 -0600

    Adds cart icon and cartIsOpen context

---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,17 @@
+import {createContext, useState} from 'react'
+
+
+export const CartContext = createContext({
+    isCartOpen: false,
+    setIsCartOpen: () => null,
+})
+
+export const CartProvider = ({children}) => {
+    const [isCartOpen, setIsCartOpen] = useState(false)
+
+    return(
+        <CartContext.Provider value={{isCartOpen, setIsCartOpen}}>
+        {children}
+        </CartContext.Provider>
+        )
+}
